613a2cb31bfacebb17f6b86894dab65f9d6baa8e
duanjl <duanjl.china@gmail.com>
2024-07-05 11:10:45 +0800

    fix: fix requestURI cannot get from default inject

--- src/main/java/stirling/software/SPDF/controller/web/HomeWebController.java
+++ src/main/java/stirling/software/SPDF/controller/web/HomeWebController.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
